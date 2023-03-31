2fa3b90637a330f0456d426033ee7a7b06badae9
Perry Werneck <perry.werneck@gmail.com>
2023-03-30 21:53:14 -0300

    Updating win32 workflow.

--- win/ci-build.sh
+++ win/ci-build.sh
@@ -23,45 +23,16 @@ cd ${myDIR}
 rm -fr ${myDIR}/.build
 
 #
-# Build LIB3270
+# Unpack LIB3270
 #
-if [ -e mingw-lib3270.tar.xz ]; then
-
-	echo "Unpacking lib3270"
-	tar -C / -Jxvf mingw-lib3270.tar.xz 
-
-else
-	echo "Building lib3270"
-	git clone https://github.com/PerryWerneck/lib3270.git ./.build/lib3270 || die "clone lib3270 failure"
-	cd ./.build/lib3270
-	./autogen.sh || die "Lib3270 autogen failure"
-	./configure || die "Lib3270 Configure failure"
-	make clean || die "Lib3270 Make clean failure"
-	make all || die "Lib3270 Make failure"
-	make install || die "Lib3270 Install failure"
-	cd ../..
-fi
+echo "Unpacking lib3270"
+tar -C / -Jxvf ${MINGW_PACKAGE_PREFIX}-lib3270.tar.xz 
 
 #
 # Build LIBV3270
 #
-if [ -e mingw-libv3270.tar.xz ]; then
-
-	echo "Unpacking libv3270"
-	tar -C / -Jxvf mingw-libv3270.tar.xz 
-
-else
-	echo "Building libv3270"
-	mkdir -p  ${myDIR}/.build/libv3270
-	git clone https://github.com/PerryWerneck/libv3270.git ./.build/libv3270 > $LOGFILE 2>&1 || die "clone libv3270 failure"
-	cd ./.build/libv3270
-	./autogen.sh > $LOGFILE 2>&1 || die "libv3270 Autogen failure"
-	./configure > $LOGFILE 2>&1 || die "libv3270 Configure failure"
-	make clean > $LOGFILE 2>&1 || die "libv3270 Make clean failure"
-	make all  > $LOGFILE 2>&1 || die "libv3270 Make failure"
-	make install  > $LOGFILE 2>&1 || die "libv3270 Install failure"
-	cd ../..
-fi
+echo "Unpacking libv3270"
+tar -C / -Jxvf ${MINGW_PACKAGE_PREFIX}-libv3270.tar.xz 
 
 #
 # Build IPC Plugin
@@ -73,9 +44,8 @@ cd ${myDIR}
 make clean > $LOGFILE 2>&1 || die "Make clean failure"
 make all  > $LOGFILE 2>&1 || die "Make failure"
 
-make DESTDIR=.bin/package install
-tar --create --xz --file=mingw-pw3270-plugin-ipc.tar.xz --directory=.bin/package --verbose .
-ls -l *.tar.xz
+make DESTDIR=.bin/package install > $LOGFILE 2>&1 || die "Install failure"
+tar --create --xz --file=${MINGW_PACKAGE_PREFIX}-pw3270-plugin-ipc.tar.xz --directory=.bin/package --verbose . > $LOGFILE 2>&1 || die "Package build failure"
 
 echo "Build complete"
 
